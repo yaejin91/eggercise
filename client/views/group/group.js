e598154bf9f17f3f99282d9580a390ce83829085
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-12-01 15:33:09 -0800

    Add angular module for group view route.

---
+++ client/views/group/group.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('eggercise')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/group', {
+        templateUrl: 'views/group/group.html',
+        controller: 'GroupCtrl',
+        controllerAs: 'vm'
+      });
+  });
