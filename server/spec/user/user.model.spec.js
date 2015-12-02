5b34a7d1c6ae1c005e1bdac3d2d7def76e9dcddc
John <you@example.com>
2015-12-01 16:02:25 -0800

    user model testing extended and passing

--- server/spec/user/user.model.spec.js
+++ server/spec/user/user.model.spec.js
@@ -1,18 +1,20 @@
-// var request = require('supertest'),
-//   User = require('../../api/user/user.model');
+var request = require('supertest'),
+  User = require('../../api/user/user.model');
 
-//   describe ('User', function() {
-//     it('should have a name and email', function() {
-//       var user = new User({name: 'test', email: 'test@test.com'});
-//       expect(user.name).toBe('test');
-//       expect(user.email).toBe('test@test.com');
-//     });
-//   });
+  describe ('User', function() {
+    it('should have a name and email', function() {
+      var user = new User({name: 'test', email: 'test@test.com'});
+      expect(user.name).toBe('test');
+      expect(user.email).toBe('test@test.com');
+    });
+  });
 
-//   describe ('User', function() {
-//     it('should have an passwordHash and salt', function() {
-//       var user = new User({name: 'blah', email: 'test@test.com', password: 'test'});
-//       expect(user.passwordHash).toBeDefined();
-//       expect(user.salt).toBeDefined();
-//     });
-//   });
+  describe ('User', function() {
+    it('should have a passwordHash and salt', function() {
+      var user = new User({name: 'blah', email: 'test@test.com', password: 'test'});
+      expect(user.passwordHash).toBeDefined();
+      expect(user.salt).toBeDefined();
+      expect(user.authenticate('test')).toBe(true);
+    });
+  });
+
