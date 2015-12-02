710226a21ffa79e358f5271c20af038843d998ca
ddsooxo <dchuteng@gmail.com>
2015-12-02 15:16:14 -0800

    fixed minor error in Invite model, invite model tests all passing

--- server/api/invite/invite.model.js
+++ server/api/invite/invite.model.js
@@ -4,14 +4,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // create a schema for Invite
-var inviteSchema = new Schema({
+var InviteSchema = new Schema({
   email: { type: String, required: true },
   _user: { type: mongoose.Schema.Types.ObjectId, ref:'User', required: false },
   _group: { type: mongoose.Schema.Types.ObjectId, ref:'Group', required: true },
   created_at: Date,
   updated_at: Date
 });
-
-var Invite = mongoose.model('Invite', inviteSchema);
 
 module.exports = mongoose.model('Invite', InviteSchema);
