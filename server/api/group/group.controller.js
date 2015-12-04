c1c8d5a35a4613fcdee41a50b0ec46fcf8e68877
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-12-04 12:50:05 -0800

    Add 1 test for group controller spec and action for group controller (show all groups).

--- server/api/group/group.controller.js
+++ server/api/group/group.controller.js
@@ -8,6 +8,19 @@ function handleError (res, err) {
   return res.status(500).send(err);
 }
 
+//Show all groups
+exports.showAll = function (req, res) {
+    Group.find({}, function (err, foundGroups) {
+        if (err) {
+            return handleError(res, err);
+        }
+        res.status(201).json({
+            group: foundGroups
+        });
+    });
+}
+
+
 //Creates a new group in the DB.
 exports.create = function (req, res) {
 	Group.create(req.body, function (err, createdGroup) {
--- server/spec/api/group/group.controller.spec.js
+++ server/spec/api/group/group.controller.spec.js
@@ -2,12 +2,12 @@
 
 //packages and modules required
 var request = require('supertest'),
-	app = require('../../../server'),
-	agent = request.agent(app);
+  app = require('../../../server'),
+  agent = request.agent(app);
 
 //model
 var Group = require('../../../api/group/group.model'),
-	User = require('../../../api/user/user.model');
+  User = require('../../../api/user/user.model');
 
 //auth
 var auth = {};
@@ -16,142 +16,160 @@ var auth = {};
 var creator;
 
 describe('Group', function() {
-	beforeAll(function (done) {
-		User.create({
-			name: 'loginDummy',
-			password: 'dummypw',
-			email: 'dummy@test.com'
-		}, function (error, dummyUser) {
-			if(error) {
-				console.log(error);
-				done.fail(error);
-			} else {
-				creator = dummyUser;
-				loginUser(auth, done);
-				// done();
-			}
-		});
-	});
+  beforeAll(function (done) {
+    User.create({
+      name: 'loginDummy',
+      password: 'dummypw',
+      email: 'dummy@test.com'
+    }, function (error, dummyUser) {
+      if(error) {
+        console.log(error);
+        done.fail(error);
+      } else {
+        creator = dummyUser;
+        loginUser(auth, done);
+        // done();
+      }
+    });
+  });
 
-	afterAll(function (done) {
-		User.remove({_id: creator._id}, function (error, removedCreator) {
-			if (error) {
-				done.fail(error);
-			} else {
-				done();
-			}
-		});
-	});
-
-	describe('with data', function() {
-		var group;
-
-		beforeEach(function (done) {
-			console.log('auth',auth);
-			Group.create({
-				name: 'testGroup',
-				email: 'testGroup@test.com',
-				bet: 100,
-				start:'12-01-2015',
-				end:'12-31-2015',
-				_creator:creator._id
-			}, function (error, newGroup) {
-				if (error) {
-					console.log(error);
-					done.fail(error);
-				} else {
-					group = newGroup;
-					done();
-				}
-			});
-		});
-
-		afterEach(function (done) {
-			Group.remove({_id: group._id}, function (error, removedGroup) {
-				if (error) {
-					done.fail(error);
-				} else {
-					done();
-				}
-			});
-		});
-
-		// it('login', loginUser());
-		
-		it('should create a new group', function (done) {
-			console.log(auth);
-			var creatorId = creator._id;
-			agent
-			.post('/api/groups/create')
-			.send({
-				name:'testGroupCreate1',
-				email: 'create@test.com',
-				bet: 9000,
-				start:'01-01-2016',
-				end: '01-31-2016',
-				_creator: creatorId
-			})
-			.set('Authorization', 'Bearer ' + auth.token)
-			.expect('Content-Type', /json/)
-			.end(function (error, res) {
-				if (error) {
-					done.fail(error);
-				} else {
-					var returnedGroup = res.body.group;
-					expect(returnedGroup.name).toBe('testGroupCreate1');
-					Group.findOne({ _id: returnedGroup._id})
-					.remove(function (error) {
-						done();
-					})
-				}
-			});
-		});			
-
-		it('should delete the group', function (done) {
-			var creatorId = creator._id;
-			agent
-			.post('/api/groups/' + group._id)
-			.set('Authorization', 'Bearer ' + auth.token)
-			.expect('Content-Type', /json/)
-			.end(function (error, res) {
-				if (error) {
-					done.fail(error);
-				} else {
-					Group.findOne({name: 'testGroup'}, function (err, deletedGroup){
-						if(err){
-							done.fail.err;
-						}else{
-							done();
-						}
-					})
-				}
-			});
-		});
+  afterAll(function (done) {
+    User.remove({_id: creator._id}, function (error, removedCreator) {
+      if (error) {
+        done.fail(error);
+      } else {
+        done();
+      }
+    });
+  });
 
 
+  describe('with no data', function () {
+    it('should return no groups', function (done) {
+      request(app).get('/api/groups')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if(err) {
+          done.fail(err);
+        } else {
+          expect(res.body).toEqual([]);
+          done();
+        }
+      });
+    });
+  });
 
-	});
+
+  describe('with data', function() {
+    var group;
+
+    beforeEach(function (done) {
+      console.log('auth',auth);
+      Group.create({
+        name: 'testGroup',
+        email: 'testGroup@test.com',
+        bet: 100,
+        start:'12-01-2015',
+        end:'12-31-2015',
+        _creator:creator._id
+      }, function (error, newGroup) {
+        if (error) {
+          console.log(error);
+          done.fail(error);
+        } else {
+          group = newGroup;
+          done();
+        }
+      });
+    });
+
+    afterEach(function (done) {
+      Group.remove({_id: group._id}, function (error, removedGroup) {
+        if (error) {
+          done.fail(error);
+        } else {
+          done();
+        }
+      });
+    });
+
+    // it('login', loginUser());
+
+    it('should create a new group', function (done) {
+      console.log(auth);
+      var creatorId = creator._id;
+      agent
+      .post('/api/groups/create')
+      .send({
+        name:'testGroupCreate1',
+        email: 'create@test.com',
+        bet: 9000,
+        start:'01-01-2016',
+        end: '01-31-2016',
+        _creator: creatorId
+      })
+      .set('Authorization', 'Bearer ' + auth.token)
+      .expect('Content-Type', /json/)
+      .end(function (error, res) {
+        if (error) {
+          done.fail(error);
+        } else {
+          var returnedGroup = res.body.group;
+          expect(returnedGroup.name).toBe('testGroupCreate1');
+          Group.findOne({ _id: returnedGroup._id})
+          .remove(function (error) {
+            done();
+          })
+        }
+      });
+    });
+
+    it('should delete the group', function (done) {
+      var creatorId = creator._id;
+      agent
+      .post('/api/groups/' + group._id)
+      .set('Authorization', 'Bearer ' + auth.token)
+      .expect('Content-Type', /json/)
+      .end(function (error, res) {
+        if (error) {
+          done.fail(error);
+        } else {
+          Group.findOne({name: 'testGroup'}, function (err, deletedGroup){
+            if(err){
+              done.fail.err;
+            }else{
+              done();
+            }
+          })
+        }
+      });
+    });
+
+
+
+  });
 });
 
 function loginUser (auth, done) {
-	agent
-	.post('/auth/local/')
-	.send({
-		email: 'dummy@test.com',
-		password: 'dummypw'
-	})
-	// .expect(200)
-	.end(onResponse);
+  agent
+  .post('/auth/local/')
+  .send({
+    email: 'dummy@test.com',
+    password: 'dummypw'
+  })
+  // .expect(200)
+  .end(onResponse);
 
-	function onResponse(error, res) {
-		if(error) {
-			console.log(error);
-			throw error;
-		} else {
-			auth.token = res.body.token;
-			agent.saveCookies(res);
-			done();
-		}
-	}
-	
+  function onResponse(error, res) {
+    if(error) {
+      console.log(error);
+      throw error;
+    } else {
+      auth.token = res.body.token;
+      agent.saveCookies(res);
+      done();
+    }
+  }
+
 }
