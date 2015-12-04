f6651df98f9cd2cba706d37514b4864b86b824cd
ddsooxo <dchuteng@gmail.com>
2015-12-04 14:34:02 -0800

    added ng click delete button and ng-model for group.html

--- client/services/group/group.spec.js
+++ client/services/group/group.spec.js
@@ -33,58 +33,38 @@
      httpBackend.verifyNoOutstandingRequest();
     });
 
-    // it('should be 0', function() {
-    //   var response = [
-    //     {
-    //       body: 'List Service Test List'
-    //     }
-    //   ];
-
-    //   // Use the httpBackend mock service to capture the call to our API and return the data we specify in the response variable
-    //   httpBackend.expectGET('/api/lists').respond(response);
-    //   // setup the service to use the success and error handler functions we defined in the beforeEach block. 
-    //   service.getLists().then(handler.success, handler.error);
-    //   // execute the HTTP API call
-    //   httpBackend.flush();
-
-    //   // test the results
-    //   expect(handler.success).toHaveBeenCalled();
-    //   expect(groupData).toEqual(response);
-    //   expect(handler.error).not.toHaveBeenCalled();
-    //   expect(errorMessage).toBeUndefined();
-    // });
-
 
     it('should delete a group', function() {
       var response = {
         group: {
-          _id : ObjectId("565f912826c74d251d0a7b04"),
-          name : "testGroup",
-          email : "testGroup@test.com",
+          
+          _id : ObjectId('565f912826c74d251d0a7b04'),
+          name : 'testGroup',
+          email : 'testGroup@test.com',
           bet : 100,
-          start : ISODate("2015-12-01T08:00:00Z"),
-          end : ISODate("2015-12-31T08:00:00Z"),
-          _creator : ObjectId("565f912726c74d251d0a7b03"),
+          start : ISODate('2015-12-01T08:00:00Z'),
+          end : ISODate('2015-12-31T08:00:00Z'),
+          _creator : ObjectId('565f912726c74d251d0a7b03'),
           _members : [ ],
           __v : 0 
         }
       }
 
+      // Use the httpBackend mock service to capture the call to our API and return the data we specify in the response variable
       httpBackend.expectPOST('/api/groups/' + group._id).respond(response);
+      // setup the service to use the success and error handler functions we defined in the beforeEach block. 
       service.deleteGroup().then(handler.success, handler.error);
+      // execute the HTTP API call
       httpBackend.flush();
 
       //tests the results
       expect(handler.success).toHaveBeenCalled();
       expect(groupData).toEqual(response);
+      expect(response.group.name).toEqual('testGroup');
       expect(handler.error).not.toHaveBeenCalled();
       expect(errorMessage).toBeUndefined();
 
     })
 
-    // it('should set current list to 1', function() {
-    //   expect(service.setList(1)).toEqual(1);
-    //   expect(service.getList()).toEqual(1);
-    // });
   });
 })();
