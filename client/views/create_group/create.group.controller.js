2178e26761024be8ff2e999676a406de732ebfef
Yae Jin Park <yaejin91@gmail.com>
2015-12-04 18:05:42 -0800

    Cleanup html templates

---
+++ client/views/create_group/create.group.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('eggercise')
+	.controller('CreateGroupCtrl', function ($log, $routeParams, GroupService) {
+		// var vm = this;
+
+		// angular.extend(vm, {
+
+		// 	name: 'CreateGroupCtrl',
+
+			
+		// });
+	});
