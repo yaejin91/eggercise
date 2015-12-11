56c284f900f4ae7f49572b4342bbb950ad43062f
Maribel Gordon <outdoorsole@users.noreply.github.com>
2015-12-11 15:13:28 -0800

    Add routes index.js file for invite model (Express server).

---
+++ server/api/invite/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var express = require('express');
+var router = express.Router();
+var controller = require('./invite.controller');
+var auth = require('../../auth/auth.service.js');
+
+module.exports = router;
