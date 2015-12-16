415c296eff7bd41ea14b656a3f5a980c05eef687
ddsooxo <dchuteng@gmail.com>
2015-12-16 11:48:30 -0800

    EmailService server side specs passing

--- server/email/email.service.js
+++ server/email/email.service.js
@@ -2,11 +2,18 @@
 var config = require('../config/environment');
 var sendgrid  = require('sendgrid')(config.sendgrid_api_key);
 
+// callback must be a function that will receive two parameters: err, json
 exports.send = function (emailTo, subject, emailText, callback) {
-  sendgrid.send({
-    to:       emailTo,
-    from:     'team@eggercise.com',
-    subject:  subject,
-    text:     emailText
-  }, callback);
+    if(emailTo){
+      sendgrid.send({
+        to:       emailTo,
+        from:     'team@eggercise.com',
+        subject:  subject,
+        text:     emailText
+      }, callback);
+    }else{ 
+        callback('Email must be provided', null);
+    }
 }
+
+
