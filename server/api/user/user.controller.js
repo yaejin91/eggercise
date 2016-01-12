7dd2424342244c2f89631bf0a640bdaedadc28fb
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-11 17:01:27 -0800

    Add a unique constraint for email address on invite model.

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var _ = require('lodash'),
-  mongoose = require('mongoose');
+mongoose = require('mongoose');
 
 var authService = require('../../auth/auth.service');
 var User = require('./user.model');
