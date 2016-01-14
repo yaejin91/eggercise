6aee348d902fb51df7572f359cbf20681334729b
Yae Jin Park <yaejin91@gmail.com>
2016-01-13 18:23:27 -0800

    spec for showWorkout service passing

--- client/services/workoutlog/workoutlog.spec.js
+++ client/services/workoutlog/workoutlog.spec.js
@@ -2,28 +2,13 @@
   'use strict';
 
   describe('WorkoutService', function() {
-    var service, $httpBackend, workoutData,handler, errorMessage;
-    var id = '5669e40077cc88a84bbb5c13';
-    var existingDate = '12-31-2015';
-    var newDate = '01-02-2016';
-    var existingUser = {
-      _id: id,
-      name: 'testlogging',
-      email: 'testlogging@test.com',
-      password: 'test',
-      exercises: [existingDate]
-    };
-    var updatedExerciseUser = {
-      _id: id,
-      name: 'testlogging',
-      email: 'testlogging@test.com',
-      password: 'test'
-    };
+    var service, $httpBackend, workoutData,handler, errorMessage, $log;
     
     // Configure module that contains the controller being tested
     beforeEach(module('eggercise'));
 
-    beforeEach(inject(function (_$httpBackend_, _WorkoutService_) {
+    beforeEach(inject(function (_$log_, _$httpBackend_, _WorkoutService_) {
+      $log = _$log_;
       $httpBackend = _$httpBackend_;
       service = _WorkoutService_;
       workoutData = [];
@@ -44,75 +29,28 @@
       spyOn(handler, 'error').and.callThrough();
     }));
 
-    //Test for logWorkout()
-    // it('should let a user log workout', function () {
-    //   id = '5669e40077cc88a84bbb5c13';     
-    //   $httpBackend.whenPOST(/log/)
-    //     .respond (respondToHttp);
+    //Test service.showWorkout()
+    it('should show all of users workout log', function () {
+      var response = service.showWorkout()
+        .then(function () {
+          $log.info('Success!');
+        })
+        .catch(function (err) {
+          $log.error('Error: ', err);
+        });
 
-    //   // setup the service to use the success and error handler functions we defined in the beforeEach block.
-    //   service.logWorkout().then(handler.success, handler.error);
-    //   $httpBackend.flush();
-      
-    //   // tests the results
-    //   expect(handler.success).toHaveBeenCalled();
-    //   expect(handler.error).not.toHaveBeenCalled();
-    //   expect(errorMessage).toBeUndefined();
-    // });
+      expect(response).toBeTruthy();
+    });
+    
 
-    // //Test for logWorkout() Fail
-    // it('should NOT let a user log workout', function () {
-    //   id = 'fakeid';
-    //   $httpBackend.whenPOST(/log/)
-    //     .respond (respondToHttp);
-
-    //   service.logWorkout().then(handler.success, handler.error);
-    //   $httpBackend.flush();
-
-    //   //test the results
-    //   expect(handler.error).toHaveBeenCalled();
-    //   expect(handler.success).not.toHaveBeenCalled();
-    //   expect(errorMessage).toBeDefined();
-    // });
-
-    // //Test for unlogWorkout()
-    // it('should let a user unlog workout', function () {
-    //   id = '5669e40077cc88a84bbb5c13';      
-    //   $httpBackend.whenPOST(/unlog/)
-    //     .respond(respondToHttp);
-
-    //   service.unlogWorkout().then(handler.success, handler.error);
-    //   $httpBackend.flush();
-      
-    //   // tests the results
-    //   expect(handler.success).toHaveBeenCalled();
-    //   expect(handler.error).not.toHaveBeenCalled();
-    //   expect(errorMessage).toBeDefined();
-    // });
-
-    // //Test for unlogWorkout() Fail
-    // it('should NOT let a user unlog workout', function () {
-    //   id = 'fakeid';
-    //   $httpBackend.whenPOST(/unlog/)
-    //     .respond (respondToHttp);
-
-    //   service.unlogWorkout().then(handler.success, handler.error);
-    //   $httpBackend.flush();
-
-    //   //test the results
-    //   expect(handler.error).toHaveBeenCalled();
-    //   expect(handler.success).not.toHaveBeenCalled();
-    //   expect(errorMessage).toBeDefined();
-    // });
-
-    function respondToHttp(method, url, params) {
-      var userId = id;
-      if (userId === existingUser._id) {
-        return [200, updatedExerciseUser];
-      } else {
-        return[404, { message: 'Not Found'}];
-      }
-    };
+    // function respondToHttp(method, url, params) {
+    //   var userId = id;
+    //   if (userId === existingUser._id) {
+    //     return [200, updatedExerciseUser];
+    //   } else {
+    //     return[404, { message: 'Not Found'}];
+    //   }
+    // };
 
   });
 })();
