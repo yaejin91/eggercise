a2894efd4e7c87ad777a7fa7dd514e160b351f01
Yae Jin Park <yaejin91@gmail.com>
2016-01-17 21:49:20 -0800

    spec for elapsedDay service passing for both cases

--- client/services/single_group/single.group.spec.js
+++ client/services/single_group/single.group.spec.js
@@ -2,14 +2,15 @@
   'use strict';
 
   describe('SingleGroupService', function () {
-    var service, handler, singleGroupData, errorMessage, $log;
+    var service, DateService, handler, singleGroupData, errorMessage, $log;
 
     //Configure module that contains the serivce being tested
     beforeEach(module('eggercise'));
 
-    beforeEach(inject(function (_$log_, _SingleGroupService_) {
+    beforeEach(inject(function (_$log_, _SingleGroupService_, _DateService_) {
       $log = _$log_;
       service = _SingleGroupService_;
+      DateService = _DateService_;
       singleGroupData = [];
 
       // Define an object with functions to handle success and error for our API calls
@@ -29,33 +30,22 @@
     }));
 
     //Test service elapsedDay() 
-    // it('should return number of days elapsed between two dates', function () {
-    //   var groupStartDate = new Date('01-01-2016');
-    //   var groupEndDate = new Date('01-04-2016');
-    //   var numberOfDays = service.elapsedDay(groupStartDate, groupEndDate)
-    //     .then(function () {
-    //       $log.info('Success!');
-    //     })
-    //     .catch(function (err) {
-    //       $log.error('Error: ', err);
-    //     });
-    //   expect(numberOfDays).toBe(3);
-    // });
+    it('should return number of days elapsed between two dates', function () {
+      var groupStartDate = new Date();
+      var groupEndDate = new Date('2016-01-31T08:00:00.000Z');
+      var numberOfDays = service.elapsedDay(groupStartDate, groupEndDate)
+        
+      expect(numberOfDays).toBeDefined();
+    });
 
     //Test service elapsedDay() when group start date is later than today's date
-    // it('should return 0 for number of days elapsed', function () {
-    //   var todayDate = new Date();
-    //   var groupStartDate = new Date('12-31-2016');
-    //   var groupEndDate = new Date('01-31-2017');
-    //   var noDaysElapsed = service.elapsedDay(groupStartDate, groupEndDate)
-    //     .then(function () {
-    //       $log.info('Success!');
-    //     })
-    //     .catch(function (err) {
-    //       $log.error('Error: ', err);
-    //     });
-    //     expect(noDaysElapsed).toBe(0);
-    // });
-
+    it('should return 0 for number of days elapsed', function () {
+      var todayDate = new Date();
+      var groupStartDate = new Date('2017-01-01T08:00:00.000Z');
+      var groupEndDate = new Date('2017-01-31T08:00:00.000Z');
+      var noDaysElapsed = service.elapsedDay(groupStartDate, groupEndDate)
+    
+        expect(noDaysElapsed).toBe(0);
+    });
   })
 })();
