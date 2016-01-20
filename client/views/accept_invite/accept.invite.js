2ed42d63806ad65fde68031d6c1e6e2e4ed2af94
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-20 12:39:53 -0800

    Add invites route to public pages, and update angular module route.

--- client/views/accept_invite/accept.invite.js
+++ client/views/accept_invite/accept.invite.js
@@ -3,9 +3,9 @@
 angular.module('eggercise')
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/invites/accept/:id', {
+      .when('/invites/accept/:invite_id', {
         //You must use " " instead of an empty string "" because Angular uses an if (template) check before firing the controller, and an empty string evaluates to false.
-        template: 'views/create_group/accept.invite.html',
+        template: 'views/accept_invite/accept.invite.html',
         controller: 'AcceptInviteCtrl',
         controllerAs: 'vm'
       });
