7c636804e3b5d2dde33a5b8e635d0f1aa460a14f
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-01-20 11:28:27 -0800

    Link the template file to then angular module for 'accept invite'.

--- client/views/accept_invite/accept.invite.js
+++ client/views/accept_invite/accept.invite.js
@@ -5,7 +5,7 @@ angular.module('eggercise')
     $routeProvider
       .when('/invites/accept/:id', {
         //You must use " " instead of an empty string "" because Angular uses an if (template) check before firing the controller, and an empty string evaluates to false.
-        template: ' ',
+        template: 'views/create_group/accept.invite.html',
         controller: 'AcceptInviteCtrl',
         controllerAs: 'vm'
       });
