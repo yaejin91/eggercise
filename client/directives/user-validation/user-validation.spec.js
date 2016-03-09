c802c12ef4a7481392d7feb58dd0f7fec25b5d18
Yae Jin Park <yaejin91@gmail.com>
2016-03-08 20:47:57 -0800

    spec written for user validation directive

---
+++ client/directives/user-validation/user-validation.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('User-Validation', function () {
+
+  beforeEach(module('eggercise'));
+
+  var element, scope;
+
+  beforeEach(inject(function ($compile, $rootScope){
+    scope = $rootScope;
+    element = angular.element();
+    $compile(element)(scope);
+    scope.digest();
+    form = scope.form;
+  }));
+
+  it('should ...', function() {
+    expect(1).toBe(1);
+  });
+});
